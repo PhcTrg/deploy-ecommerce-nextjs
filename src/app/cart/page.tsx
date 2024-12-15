7fcc5a64474f1e2dc5596d0b4617205ec1fb6650
Phước Trường <152671615+PhcTrg@users.noreply.github.com>
2024-12-15 18:07:41 +0700

    fix

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,11 @@
 import dynamic from "next/dynamic";
 
-const CartContent = dynamic(() => import("@/components/cart-content"), {
-  ssr: false,
-});
+const CartContent = dynamic(
+  () => import("@/components/cart-content").then((mod) => mod.default),
+  {
+    ssr: false,
+  }
+);
 
 export default function CartPage() {
   return <CartContent />;
--- src/components/cart-content.tsx
+++ src/components/cart-content.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import cartAPIs from "@/api/cart";
-import Link from "next/link";
-import { useDisclosure } from "@nextui-org/react";
-import ResultModal from "@/components/modal/result-modal";
-import { useRouter } from "next/navigation";
+import React from "react";
 
 const CartContent = () => {
-  // ... paste all the content from the previous CartPage component here ...
-  // (everything from const [isLoading, setIsLoading] to the end of the component)
+  return <div>{/* Your cart content here */}</div>;
 };
 
 export default CartContent;
