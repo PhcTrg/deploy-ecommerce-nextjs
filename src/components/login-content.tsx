7913bef2abb71dcff61658a792579d8fc9a889f8
Phước Trường <152671615+PhcTrg@users.noreply.github.com>
2024-12-15 18:05:46 +0700

    fix

---
+++ src/components/login-content.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const LoginContent = () => {
+  return (
+    <div>
+      {/* Add your login form content here */}
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default LoginContent;
---
+++ src/components/profile-content.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const ProfileContent = () => {
+  return (
+    <div>
+      {/* Add your profile page content here */}
+      <h1>Profile Page</h1>
+    </div>
+  );
+};
+
+export default ProfileContent;
